a5d87b1b4aa68fa38e1d29d8eb484db48fc3689e
Thabsheer <thabsheerporuthiyil@gmail.com>
2025-10-10 10:05:42 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -26,14 +26,14 @@ function App() {
 
   return (
     <div className="flex flex-col min-h-screen">
-    
+      <ScrollToTop/>
       {!isAdminRoute && <Navbar />}
 
       <ToastContainer position="top-center" autoClose={2000} />
 
       <main className="flex-grow">
         <Routes>
-          <ScrollToTop/>
+          
           <Route path="/" element={<Home />} />
           <Route path="/watches" element={<Watches />} />
           <Route path="/login" element={<Login />} />
