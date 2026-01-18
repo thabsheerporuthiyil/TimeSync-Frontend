4b74a86ba98eb642e76b45ba9995952642d53c4f
Thabsheer <thabsheerporuthiyil@gmail.com>
2026-01-18 21:58:52 +0530

    Apply sentry

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-// import { AuthProvider } from "./context/AuthContext";
 import { UserProvider } from "./context/UserContext.jsx";
 import { ShopProvider } from "./context/ShopContext.jsx";
 import { BrowserRouter } from "react-router-dom";
@@ -9,13 +7,31 @@ import "./index.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import { NotificationProvider } from "./context/NotificationContext.jsx";
 
+// Import Sentry
+import * as Sentry from "@sentry/react";
+
+// Initialize Sentry
+Sentry.init({
+  dsn: import.meta.env.VITE_SENTRY_DSN,
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, 
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0, 
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <BrowserRouter>
       <UserProvider>
         <NotificationProvider>
           <ShopProvider>
-            <App />
+            <Sentry.ErrorBoundary fallback={<p>An error has occurred</p>}>
+               <App />
+            </Sentry.ErrorBoundary>
           </ShopProvider>
         </NotificationProvider>
       </UserProvider>
